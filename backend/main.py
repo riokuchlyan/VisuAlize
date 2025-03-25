31153898fb1e089c53ed4b1d975de8f6553b7ccc
Rio Kuchlyan <riokuchlyan@gmail.com>
2025-03-25 14:32:07 -0400

    Remove startup message for server port

--- backend/main.py
+++ backend/main.py
@@ -37,5 +37,4 @@ async def root(req: Request):
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000)) 
-    print(f"Starting server on port {port}...") 
     uvicorn.run(app, host="0.0.0.0", port=port)
