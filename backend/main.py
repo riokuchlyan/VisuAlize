9308cccd7516cb1821541f06c416100340301b76
Rio Kuchlyan <riokuchlyan@gmail.com>
2025-03-25 15:37:55 -0400

    Add production URL to CORS allowed origins

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://visualize-navy.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
