57a565100385211afab916a3e60b2c15bca2f776
Rio Kuchlyan <riokuchlyan@gmail.com>
2025-04-03 02:47:16 -0400

    Remove globals folder

--- backend/globals/environment.py
+++
@@ -1,6 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict # type: ignore
-
-class Environment(BaseSettings):
-    TOGETHER_AI_KEY: str
-
-    model_config: SettingsConfigDict = SettingsConfigDict(env_file='.env')
