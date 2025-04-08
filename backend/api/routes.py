695e7d9d209c64e8d157f698130d8904452a6190
Rio Kuchlyan <riokuchlyan@gmail.com>
2025-04-08 13:39:06 -0400

    Temporarily remove type hints

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,3 +1,4 @@
+# internal
 from handlers import *
 
 # external
@@ -21,7 +22,7 @@ class InputRequest(BaseModel):
     input: str
 
 @router.get("/submissions")
-async def get_submissions() -> dict:
+async def get_submissions():
     return handle_get_submissions(latest_ticker)
 
 @router.post("/ticker")
@@ -49,35 +50,35 @@ def get_latest_input() -> Dict[str, str]:
     return {"error": "No input submitted"}
 
 @router.get("/ai_analysis")
-def get_analysis() -> dict:
+def get_analysis():
     return handle_get_ai_analysis(latest_ticker, latest_input)
 
 @router.get("/filings_link", response_class=PlainTextResponse)
-def get_filings() -> str:    
+def get_filings():    
     return handle_get_filings_link(latest_ticker)
 
 @router.get("/basic_data")
-def basic_data() -> dict:
+def basic_data():
     return handle_get_basic_data(latest_ticker)
 
 @router.get("/sentiment")
-def get_sentiment() -> dict:
+def get_sentiment():
     return handle_get_sentiment(latest_ticker)
 
 @router.get("/technicals")
-def get_technicals() -> dict:
+def get_technicals():
     return handle_get_ai_analysis_technicals(latest_ticker)
 
 @router.get("/valuation")
-def get_valuation() -> dict:
+def get_valuation():
     return handle_get_ai_analysis_valuation(latest_ticker)
 
 @router.get("/stock_data")
-def stock_data() -> dict:
+def stock_data():
     data = handle_get_stock_data(latest_ticker)
     return json.loads(data)
 
 @router.get("/news")
-def get_news() -> dict:
+def get_news():
     data = handle_get_news_data(latest_ticker)
     return json.loads(data)
