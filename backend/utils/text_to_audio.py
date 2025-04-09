90721faf053af166b2816dd2e9be1b6e58cd9ba9
Rio Kuchlyan <riokuchlyan@gmail.com>
2025-04-09 01:34:42 -0400

    Update text_to_audio.py

--- backend/utils/text_to_audio.py
+++ backend/utils/text_to_audio.py
@@ -10,6 +10,7 @@ import base64
 def text_to_audio(text):
     tts = gTTS(text=text, lang='en')
     temp_data = io.BytesIO()
+    tts.write_to_fp(temp_data)
     temp_data.seek(0)
     encoded_data = base64.b64encode(temp_data.read()).decode("utf-8")
     return encoded_data
