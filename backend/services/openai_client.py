d262fbe0cb64c9ae888ffd9d1af32f9299ea3a3a
Rio Kuchlyan <riokuchlyan@gmail.com>
2025-04-14 19:01:30 -0400

    Edit open_ai responses to remove new line characters

--- backend/services/openai_client.py
+++ backend/services/openai_client.py
@@ -5,7 +5,6 @@ from config import OPEN_AI_KEY
 from openai import OpenAI 
 
 # built-in
-import os
 
 client = OpenAI(
   api_key=OPEN_AI_KEY
@@ -19,7 +18,7 @@ def get_ai_response(data: str, input_text: str) -> str:
         {"role": "user", "content": f"You must answer strictly based on the provided information and by looking up the given companies 10-k form. Do not use external knowledge, make assumptions, or infer anything beyond what is explicitly stated besides the companies 10-k form that you can look up. Here is the information you must use: {data}, Now, answer the following question based only on the above information and the companies 10-k form:{input_text}. You must provide the answers in an easy to read and digestable format with just text. Only use plain text so do not format your answer using characters such as the new line character. For example, in your response ensure it can be interpreted in plain text and not have characters such as slash n appear. Under no circumstances should your response be over 1000 characters."}
     ]
     )
-    return (completion.choices[0].message.content)
+    return (completion.choices[0].message.content.replace('\n', ''))
 
 def get_ai_response_technicals(data: str) -> str:
     completion = client.chat.completions.create(
@@ -29,7 +28,7 @@ def get_ai_response_technicals(data: str) -> str:
         {"role": "user", "content": f"Give me technical data and key performance indicators such as P/E ratios and more for this stock: {data}, You must provide the answers in an easy to read and digestable format with just text. Only use plain text so do not format your answer using characters such as the new line character. For example, in your response ensure it can be interpreted in plain text and not have characters such as slash n appear. Under no circumstances should your response be over 500 characters."}
     ]
     )
-    return (completion.choices[0].message.content)
+    return (completion.choices[0].message.content.replace('\n', ''))
 
 def get_ai_response_valuation(data: str) -> str:
     completion = client.chat.completions.create(
@@ -39,7 +38,7 @@ def get_ai_response_valuation(data: str) -> str:
         {"role": "user", "content": f"Make me the appropriate models for this stock: {data}, such as a DCF and more. You must provide the answers in an easy to read and digestable format with just text. Only use plain text so do not format your answer using characters such as the new line character. For example, in your response ensure it can be interpreted in plain text and not have characters such as slash n appear. Under no circumstances should your response be over 500 characters."}
     ]
     )
-    return (completion.choices[0].message.content)
+    return (completion.choices[0].message.content.replace('\n', ''))
 
 def get_ai_response_summary(data: str) -> str:
     completion = client.chat.completions.create(
@@ -49,4 +48,4 @@ def get_ai_response_summary(data: str) -> str:
         {"role": "user", "content": f"Summarize the following data in a format so that text to speech can be applied to it. The following data is finanical data, so summarize it in a way a news reporter would do it or so: {data}. Under no circumstances should your response be over 1500 characters."}
     ]
     )
-    return (completion.choices[0].message.content)
+    return (completion.choices[0].message.content.replace('\n', ''))
